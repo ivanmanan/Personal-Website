96957ecd00b9dcd1a8a7476d9f20aeb43d829688
Ivan Manan <ivanmanan@gmail.com>
2017-07-21 22:32:12 -0700

    Created first jsx file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // UI jsx file
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+
+// Probably export the objects from other js files with good descriptions of them and as objects
+var dummyArray = [ "Bio", "Mad Manan Movie Review", "Emacs Init Configuration" ];
 
 
 // Render onto web page
 ReactDOM.render(
-	<div />,
-    document.getElementById('root')
+    <App />,
+    document.getElementById("root")
 );
