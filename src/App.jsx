6d3a4848c17c6c23ab27fa3d8bbc501fac3406b9
ivanmanan <ivanmanan@gmail.com>
2017-12-17 17:48:52 -0800

    Re-arranged order

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Bucket from "./Components/Bucket.jsx";
+import Project from "./Components/Project.jsx";
 
 // This file passes down all the info to the other templating
 // Every new project: Make sure to rearrange start/end indices
@@ -20,22 +20,30 @@ class App extends Component {
 		    info: "Traveling enthusiast. Favorite place: Fingal's Cave.",
 		    index: "start"
 	    },
+	    {
+		    title: "Travel Share",
+		    photo: "/images/construction.jpeg",
+        // Need to replace link with http://travelsharego.com
+		    link: "https://github.com/ivanmanan/Travel-Share",
+		    overview: "Project currently in construction. Expected completion end of December.",
+		    info: "Document your travels and share with others.",
+		    index: "1"
+	    },
       {
 		    title: "Family Watch",
 		    photo: "/images/family-watch.png",
 		    link: "https://www.youtube.com/watch?v=kG9u_v0YBbw",
 		    overview: "Suspect your children skipping school? Now you can track their location!",
 		    info: "When logged in, the application tracks GPS and displays location history of other users.",
-		    index: "1"
+		    index: "2"
       },
 	    {
 		    title: "FrackMan",
-		    // Photo will be a screenshot of my project
 		    photo: "/images/FrackMan.png",
 		    link: "https://github.com/ivanmanan/FrackMan",
 		    overview: "Video game programmed in C++.",
 		    info: "Score as many points as possible by collecting oil barrels while avoiding protesters.",
-		    index: "2"
+		    index: "3"
 	    },
 	    {
 		    title: "Doctor Crux",
@@ -43,15 +51,6 @@ class App extends Component {
 		    link: "https://github.com/ivanmanan/Doctor-Crux",
 		    overview: "Self-diagnosis web application.",
 		    info: "List your symptoms, and Doctor Crux will suggest diagnoses and treatments.",
-		    index: "3"
-	    },
-	    {
-		    title: "JumbleXplore",
-		    photo: "/images/construction.jpeg",
-		    link: "https://github.com/ivanmanan/JumbleXplore",
-		    overview: "Project currently in construction. Expected completion end of this Winter.",
-        // overview: A social media platform for traveling enthusiasts.",
-		    info: "Document your travels and share with others.",
 		    index: "4"
 	    },
       {
@@ -67,7 +66,7 @@ class App extends Component {
   render() {
 	  return (
 	    <div className="App">
-		    <Bucket projects={this.state.projects} />
+		    <Project projects={this.state.projects} />
 	    </div>
 	  );
   }
---
+++ src/Components/Project.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ProjectItem from "./ProjectItem";
+
+// Template for containers
+class Project extends Component {
+  render() {
+	  let projectItems;
+
+	  // Map the projects
+	  if(this.props.projects) {
+	    projectItems = this.props.projects.map(project => {
+		    return (
+		      <ProjectItem key={project.index} project={project} />
+		    );
+	    });
+	  }
+
+	  return (
+	    <div className="Project">
+		    {projectItems}
+	    </div>
+	  );
+  }
+}
+
+export default Project;
