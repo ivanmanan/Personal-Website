93d37cbea5f76a0b586324913087f43daf13853b
ivanmanan <ivanmanan@gmail.com>
2017-12-29 13:09:47 -0800

    Updated to move travelshare to incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,13 @@ class App extends Component {
 		    info: "Traveling enthusiast. Favorite place: Fingal's Cave.",
 		    index: "start"
 	    },
-	    {
-		    title: "Travel Share",
-		    photo: "/images/travelshare.png",
-            // Need to replace link with http://travelsharego.com
-		    link: "https://github.com/ivanmanan/Travel-Share",
-		    overview: "Project currently in construction. Expected completion end of December.",
-		    info: "Document your travels and share with others.",
-		    index: "1"
-	    },
       {
 		    title: "Family Watch",
 		    photo: "/images/family-watch.png",
 		    link: "https://www.youtube.com/watch?v=kG9u_v0YBbw",
 		    overview: "Suspect your children skipping school? Now you can track their location!",
 		    info: "When logged in, the application tracks GPS and displays location history of other users.",
-		    index: "2"
+		    index: "1"
       },
 	    {
 		    title: "FrackMan",
@@ -43,7 +34,7 @@ class App extends Component {
 		    link: "https://github.com/ivanmanan/FrackMan",
 		    overview: "Video game programmed in C++.",
 		    info: "Score as many points as possible by collecting oil barrels while avoiding protesters.",
-		    index: "3"
+		    index: "2"
 	    },
 	    {
 		    title: "Doctor Crux",
@@ -51,13 +42,21 @@ class App extends Component {
 		    link: "https://github.com/ivanmanan/Doctor-Crux",
 		    overview: "Self-diagnosis web application.",
 		    info: "List your symptoms, and Doctor Crux will suggest diagnoses and treatments.",
+		    index: "3"
+	    },
+      {
+		    title: "Travel Share",
+		    photo: "/images/travelshare.png",
+		    link: "#",
+		    overview: "Project currently in construction.",
+		    info: "Document your travels and share with others.",
 		    index: "4"
 	    },
       {
         title: "UAS",
 		    photo: "/images/construction.jpeg",
-		    link: "https://github.com/uas-at-ucla/suas_2018/tree/master/ground",
-		    overview: "Project currently in construction. This is my team's source code for our autonomous drone.",
+		    link: "#",
+		    overview: "Project currently in construction. Repository has been privated.",
 		    info: "Will be directing the ground station team for the June 2018 AUVSI SUAS competition.",
 		    index: "end"
       }
