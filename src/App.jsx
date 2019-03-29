c8b45ae0b5ee9516e6a7b2c54ef1bd1cf10a405a
Ivan Manan <ivanmanan@gmail.com>
2019-03-28 18:53:39 -0700

    Added tad the therapist project

--- src/App.jsx
+++ src/App.jsx
@@ -11,55 +11,56 @@ class App extends Component {
 	}
   }
   componentWillMount() {
-	this.setState({projects: [
-	  {
-		title: "Bio",
-		photo: "/images/new-ivan.jpg",
-		link: "#",
-		overview: "Student at UCLA and aspiring software engineer.",
-		info: "Traveling enthusiast. Favorite place: Fingal's Cave.",
-		index: "start"
-	  },
-      {
-		title: "Travel Share",
-		photo: "/images/travelshare.png",
-		link: "http://travelsharego.live",
-		linkInfo: "See Application Live",
-		info: "Document your travels and share with others.",
-		index: "1"
-	  },
-      {
-		title: "Family Watch",
-		photo: "/images/family-watch.png",
-		link: "https://www.youtube.com/watch?v=kG9u_v0YBbw",
-		linkInfo: "Project Video",
-		info: "When logged in, the application tracks GPS and displays location history of other users.",
-		index: "2"
-      },
-	  {
-		title: "FrackMan",
-		photo: "/images/FrackMan.png",
-		link: "https://github.com/ivanmanan/FrackMan",
-		linkInfo: "GitHub Repostitory",
-		info: "Score as many points as possible by collecting oil barrels while avoiding protesters.",
-		index: "3"
-	  },
-	  {
-		title: "Doctor Crux",
-		photo: "/images/Crux.png",
-		link: "https://github.com/ivanmanan/Old-Doctor-Crux",
-		linkInfo: "Github Repository",
-		info: "List your symptoms, and Doctor Crux will suggest diagnoses and treatments.",
-		index: "4"
-	  },
-      {
-        title: "UAS",
-		photo: "/images/uas.jpg",
-		link: "https://youtu.be/CuTlgT7vdks",
-		linkInfo: "UAS Video",
-		info: "Directed the ground station team for the June 2018 AUVSI SUAS competition.",
-		index: "end"
-      }
+	  this.setState({projects: [{
+		  title: "Bio",
+		  photo: "/images/new-ivan.jpg",
+		  link: "#",
+		  overview: "Student at UCLA and aspiring software engineer.",
+		  info: "Traveling enthusiast. Favorite place: Fingal's Cave.",
+		  index: "start"
+	  }, {
+      title: "Tad the Therapist",
+      photo: "/images/tad.png",
+      link: "http://github.com/ivanmanan/Tad-The-Therapist",
+      linkInfo: "GitHub Source Code",
+      info: "Tad is a virtual assistant that provides counseling. Tad utilizes speech signal processing and hidden Markov models for automatic speech recognition.",
+      index: "1"
+    }, {
+		  title: "Travel Share",
+		  photo: "/images/travelshare.png",
+		  link: "http://travelsharego.live",
+		  linkInfo: "See Application Live",
+		  info: "Document your travels and share with others.",
+		  index: "2"
+	  }, {
+		  title: "Family Watch",
+		  photo: "/images/family-watch.png",
+		  link: "https://www.youtube.com/watch?v=kG9u_v0YBbw",
+		  linkInfo: "Project Video",
+		  info: "When logged in, the application tracks GPS and displays location history of other users.",
+		  index: "3"
+    }, {
+		  title: "FrackMan",
+		  photo: "/images/FrackMan.png",
+		  link: "https://github.com/ivanmanan/FrackMan",
+		  linkInfo: "GitHub Source Code",
+		  info: "Score as many points as possible by collecting oil barrels while avoiding protesters.",
+		  index: "4"
+	  }, {
+		  title: "Doctor Crux",
+		  photo: "/images/Crux.png",
+		  link: "https://github.com/ivanmanan/Old-Doctor-Crux",
+		  linkInfo: "Github Repository",
+		  info: "List your symptoms, and Doctor Crux will suggest diagnoses and treatments.",
+		  index: "5"
+	  }, {
+      title: "UAS",
+		  photo: "/images/uas.jpg",
+		  link: "https://youtu.be/CuTlgT7vdks",
+		  linkInfo: "UAS Video",
+		  info: "Directed the ground station team for the June 2018 AUVSI SUAS competition.",
+		  index: "end"
+    }
 	]});
   }
   render() {
